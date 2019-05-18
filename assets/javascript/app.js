8abd3b3668b798de5a1b521d7ffe0dae76642cc9
Kim Graff <kimagraff@gmail.com>
2019-05-18 12:46:49 -0400

    starting java

---
+++ assets/javascript/app.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+//SETS UP TRIVIA
+    $("#remaining-time").hide();
+    $("#start").on('click', trivia.startGame);
+    $(document).on('click' , '.option', trivia.guessChecker);
+    
+  })
+  
+  var trivia = {
+    //FOR RESET TRIVIA
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    currentSet: 0,
+    timer: 30,
+    timerOn: false,
+    timerId : '',
+    // QUESTIONS
+    questions: {
+      q1: 'Which of these is likely to be lacking in a vegan diet?',
+      q2: 'A vegan or vegetarian diet is bound to be healthy.',
+      q3: 'Beans and legumes are plant-based sources of iron. Which of these contains the most iron?',
+      q4: 'Which of these ‘junk food’ items happens to be accidentally vegan?',
+
+    },
+    //CREATE ANSWER ARRAY
+    options: {
+      q1: ['Viatmin E', 'Vitamin C', 'Vitamin B12', 'Fiber'],
+      q2: ['True', 'False', 'Idk', 'Omivore FTW'],
+      q3: ['kidney beans', 'lentins', 'lima beans', 'chickpeas'],
+      q4: ['Hersheys Chocolate Syrup', 'Puff Cheeots', 'Cherry Poptarts', 'Greek Yogurt'],
+    },
+    //ESTABLISHES ANSWERS
+    answers: {
+      q1: 'Vitamin B12',
+      q2: 'False',
+      q3: 'lentils',
+      q4: 'Hersheys Chocolate Syrup',
+    },
+    // TO START GAME
+    startGame: function(){
+      // RESETTING RESULTS TO BASIC 
+      trivia.currentSet = 0;
+      trivia.correct = 0;
+      trivia.incorrect = 0;
+      trivia.unanswered = 0;
+      clearInterval(trivia.timerId);
+      
+      // SHOW GAME
+      $('#game').show();
+      
+      //  REMOVE LAST RESULTS
+      $('#results').html('');
+      
+      // SHOW TIMER
+      $('#timer').text(trivia.timer);
+      
+      // HIDES START BUTTON
+      $('#start').hide();
+  
+      $('#remaining-time').show();
+      
+      // ASK QUESTION 
+      trivia.nextQuestion();
+      
+    },
+
+    
